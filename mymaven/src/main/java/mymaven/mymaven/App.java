811d010ad1f4ca55b236cabe4d362d446415f41a
srisha007 <srisha.aithal@gmail.com>
2020-03-27 12:01:40 +0530

    comments 27 march 2020

---
+++ mymaven/src/main/java/mymaven/mymaven/App.java
@@ -0,0 +1,13 @@
+package mymaven.mymaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
